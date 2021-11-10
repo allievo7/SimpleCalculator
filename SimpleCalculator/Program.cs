c30a116c7144e9d04e2b1e7c5df661cfeeb8295e
allievo7 <93972864+allievo7@users.noreply.github.com>
2021-11-10 11:59:53 +0100

    scritto il codice

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -6,7 +6,12 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string numero1 = args[0];
+            int numero1int = Convert.ToInt32(numero1);
+            string numero2 = args[1];
+            int numero2int = Convert.ToInt32(numero2);
+            int somma = numero1int + numero2int;
+            Console.WriteLine("la somma di " + numero1 + " + " + numero2  + " è uguale a: " + somma);
         }
     }
 }
